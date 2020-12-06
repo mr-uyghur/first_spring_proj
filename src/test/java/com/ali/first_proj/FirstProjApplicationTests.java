29316f02a745480cb602233da300161dc6ec5da5
alimran halmamat <mr.uygur1996@gmail.com>
2020-12-05 20:35:53 -0800

    spring proj commit

---
+++ src/test/java/com/ali/first_proj/FirstProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ali.first_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
